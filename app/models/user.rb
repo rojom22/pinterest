93aad9830b2324a55665beafa1f855dd441e9650
rojom22 <rojobeijing@gmail.com>
2022-10-29 01:02:39 +0300

    Finish user profile avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,10 @@ class User < ApplicationRecord
   has_many :pins
 
   validates_uniqueness_of :username
+
+  has_one_attached :avatar
+
+  def avatar_thumbnail
+    avatar.variant(resize_to_limit: [150, 150]).processed
+  end
 end
